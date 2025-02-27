5e66c26456764f93a08646fb0b1f9a436ce158c6
laura <goeres28@up.edu>
2025-02-26 19:13:54 -0800

    imported the drawables and copy-pasted layout from project hell.

--- app/src/main/java/edu/up/cs301/bahbl/BAHBLPokeangel.java
+++ app/src/main/java/edu/up/cs301/bahbl/BAHBLPokeangel.java
@@ -11,16 +11,7 @@ import android.widget.TextView;
 
 
 /**
-* A computer-version of a counter-player.  Since this is such a simple game,
-* it just sends "+" and "-" commands with equal probability, at an average
-* rate of one per second. This computer player does, however, have an option to
-* display the game as it is progressing, so if there is no human player on the
-* device, this player will display a GUI that shows the value of the counter
-* as the game is being played.
-* 
-* @author Steven R. Vegdahl
-* @author Andrew M. Nuxoll
-* @version September 2013
+*
 */
 public class BAHBLPokeangel extends BAHBLCustomerBase {
 	
