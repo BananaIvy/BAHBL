c501682593e2b580518f63e2444be872fecf71cc
alex <bakera27@up.edu>
2025-02-26 19:15:34 -0800

    Added the instance variables as well as constructor for the customer base

--- app/src/main/java/edu/up/cs301/bahbl/BAHBLCustomerBase.java
+++ app/src/main/java/edu/up/cs301/bahbl/BAHBLCustomerBase.java
@@ -1,7 +1,34 @@
 package edu.up.cs301.bahbl;
 
+
 public abstract class BAHBLCustomerBase {
 
 
+    //Instance Variables
+    private String CustomerName;
+    private int likeability;
+    private static int idNumber = 0;
+    private boolean haveGiven;
+    private boolean haveReceived;
+    private String goodResponse;
+    private String badResponse;
 
+
+    //creates a CustomerBase object
+    public BAHBLCustomerBase (String name){
+        CustomerName = name;
+        likeability = 0;
+        idNumber++;
+        boolean haveGiven = false;
+        boolean haveReceived = false;
+        String[] dialogue = new String[5];
+        //dialogue = {"lore", "greeting", "initial dialogue request", "item response", "goodbye"};
+    }
+
+
+    //getItem method
+    public int getItem (){
+        /* IMPLEMENT THIS AFTER */
+        return 0;
+    }
 }
