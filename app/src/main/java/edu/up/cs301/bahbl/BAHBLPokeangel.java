add9e35bc47c0aaf1c82aa434232aa8436ebb860
laura <goeres28@up.edu>
2025-02-28 13:25:40 -0800

    less placeholder-y pokeangel lore text

--- app/src/main/java/edu/up/cs301/bahbl/BAHBLPokeangel.java
+++ app/src/main/java/edu/up/cs301/bahbl/BAHBLPokeangel.java
@@ -26,7 +26,7 @@ public class BAHBLPokeangel extends BAHBLCustomerBase {
 				"A puppy can go through the Green Glass Door, but a dog cannot. A school can go through the Green Glass Door, but a house cannot. Which of these can go through the Green Glass Door?");
 		setHappyResponse("Indeed! You are wise beyond your years, mortal. May you bloom and bear rich fruit ere your time here comes to an end.");
 		setMadResponse ("Alas, you disappoint me. I am afraid I must depart. Your presence sickens me.");
-		setLoreDialogue("My phone poofed.");
+		setLoreDialogue("Alas, I have fallen from grace, like a gently floating petal on the wind. I must earn my way back to my eternal redemption.");
 		setFarewellDialogue("May the ruler and overlord of all look kindly on you, mortal. Your current soul is too scarred to find rest here.");
 		setGoodButtonText("A pepper");
 		setBadButtonText("A piper");
