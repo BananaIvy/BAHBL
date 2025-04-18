506adac32cf3e2a03cbb113677cf37522334c2e7
greenup28 <greenup28@up.edu>
2025-04-17 20:55:48 -0700

    issue about the after trivia dialog

--- app/src/main/java/edu/up/cs301/bahbl/BahLocalGame.java
+++ app/src/main/java/edu/up/cs301/bahbl/BahLocalGame.java
@@ -419,7 +419,7 @@ public class BahLocalGame extends LocalGame {
 
     public boolean actTrivia(GameAction action){
         //trivia button
-        gameState.setQuestionsAnwsered((gameState.getQuestionsAnwsered() + 1));
+
 
         //The correct answers go in the order 2, 4, 1, 4, 2
 
@@ -433,6 +433,7 @@ public class BahLocalGame extends LocalGame {
 
                 }
 
+                gameState.setQuestionsAnwsered((gameState.getQuestionsAnwsered() + 1));
                 gameState.setTriviaButtonClicked(true);
 
                 //say it was a legal move
@@ -446,12 +447,14 @@ public class BahLocalGame extends LocalGame {
                     gameState.setCorrectAnswersCount(gameState.getCorrectAnswersCount() + 1);
                 }
 
+                gameState.setQuestionsAnwsered((gameState.getQuestionsAnwsered() + 1));
                 gameState.setTriviaButtonClicked(true);
 
                 return true;
             }
             else if (((BahActionTriviaButton) action).getWhichButton() == 3) {
 
+                gameState.setQuestionsAnwsered((gameState.getQuestionsAnwsered() + 1));
                 gameState.setCorrectAnswer(false);
                 gameState.setTriviaButtonClicked(true);
 
@@ -461,11 +464,13 @@ public class BahLocalGame extends LocalGame {
 
                 if ((gameState.getTriviaSection() == 1) || (gameState.getTriviaSection() == 3)) {
 
+
                     gameState.setCorrectAnswer(true);
                     gameState.setCorrectAnswersCount(gameState.getCorrectAnswersCount() + 1);
 
                 }
 
+                gameState.setQuestionsAnwsered((gameState.getQuestionsAnwsered() + 1));
                 gameState.setTriviaButtonClicked(true);
                 return true;
             }
@@ -482,6 +487,7 @@ public class BahLocalGame extends LocalGame {
                 if (gameState.getQuestionsAnwsered() == 5){
 
                     gameState.setGameTime(false);
+                    customer.setPlayersTurn(false);
 
                 }
 
@@ -504,6 +510,8 @@ public class BahLocalGame extends LocalGame {
                     }
 
                     gameState.setGameTime(false);
+                    customer.setPlayersTurn(false);
+
 
                 }
 
